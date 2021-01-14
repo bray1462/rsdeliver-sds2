f8d3cd2663d693f126a706309fd0174e30a98d8f
unknown <josealexsilva007@gmail.com>
2021-01-14 20:14:14 -0300

    project created

---
+++ backend/src/main/java/com/devsuperior/rsdeliver/RsdeliverApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.rsdeliver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RsdeliverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RsdeliverApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devsuperior/rsdeliver/RsdeliverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.rsdeliver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RsdeliverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
